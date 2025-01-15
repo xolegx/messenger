7fc829c690ee9c444af430869bcba897cf377be9
xoleg <xolegx89@gmail.com>
2025-01-16 00:20:34 +0500

    'notific'

--- app/chat/router.py
+++ app/chat/router.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Request, Depends
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import List, Dict
 from app.chat.core import MessagesCORE
 from app.chat.schemas import MessageRead, MessageCreate
+from app.chat.models import Message
 from app.users.core import UsersCORE
 from app.users.dependencies import get_current_user
 from app.users.models import User
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import async_session_maker
+from sqlalchemy.future import select
+
 import asyncio
 
 router = APIRouter(prefix='/chat', tags=['Chat'])
@@ -56,8 +60,39 @@ async def send_message(message: MessageCreate, current_user: User = Depends(get_
         'recipient_id': message.recipient_id,
         'content': message.content,
     }
-
-    # Проверяем, подключен ли получатель
     await notify_user(message.recipient_id, message_data)
     await notify_user(current_user.id, message_data)
     return {'recipient_id': message.recipient_id, 'content': message.content, 'status': 'ok', 'msg': 'Message saved!'}
+
+
+@router.put("/messages/{message_id}/read")
+async def mark_as_read(message_id: int):
+    async with async_session_maker() as session:
+        result = await session.execute(
+            select(Message).filter(Message.id == message_id)
+        )
+        message = result.scalars().first()
+
+        if not message:
+            raise HTTPException(status_code=404, detail="Message not found")
+
+        message.is_read = True
+        await session.commit()
+        await session.refresh(message)
+
+    return message
+
+
+@router.get("/messages/unread/{user_id}")
+async def count_unread_messages(user_id: int):
+    async with async_session_maker() as session:
+        result = await session.execute(
+            select(Message).filter(
+                Message.recipient_id == user_id,
+                Message.is_read == False
+            )
+        )
+        unread_messages = result.scalars().all()  # Получаем все непрочитанные сообщения
+        count = len(unread_messages)  # Подсчитываем количество непрочитанных сообщений
+
+    return {"unread_count": count}
