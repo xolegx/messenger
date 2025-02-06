636597a8f00b132c6d80eb22fe54391ab03aa280
xoleg <xolegx89@gmail.com>
2025-02-06 23:59:06 +0500

    'edit_uploadfile'

--- app/files/router.py
+++ app/files/router.py
@@ -1,15 +1,14 @@
-from fastapi import APIRouter, UploadFile, File as FastAPIFile, Depends
-from typing import List
-import shutil
 import os
-from app.files.schemas import FileRead
-from app.database import async_session_maker
-from app.files.models import File
+from fastapi import APIRouter, UploadFile, Depends
+from typing import List
 from sqlalchemy.future import select
 from sqlalchemy import or_
+from app.database import async_session_maker
 from app.users.models import User
-from app.chat.models import Message
 from app.users.dependencies import get_current_user
+from app.files.schemas import FileRead
+from app.files.models import File
+
 
 router = APIRouter(prefix='/files', tags=['File'])
 
@@ -19,17 +18,14 @@ if not os.path.exists(UPLOAD_DIRECTORY):
     os.makedirs(UPLOAD_DIRECTORY)
 
 
-@router.post("/upload/", response_model=FileRead)
-async def upload_file(file: UploadFile = FastAPIFile(...), message_id: int = None, sender_id: int = None, recipient_id: int = None,
-                      current_user: User = Depends(get_current_user)):
-    file_location = f"{UPLOAD_DIRECTORY}/{file.filename}"
-
-    # Сохранение файла на диск
-    with open(file_location, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+@router.post("/upload-file/")
+async def upload_file(file: UploadFile, message_id: int, recipient_id: int, current_user: User = Depends(get_current_user)):
+    file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
+    with open(file_path, "wb") as buffer:
+        buffer.write(await file.read())
 
     async with async_session_maker() as session:
-        db_file = File(filename=file.filename, file_url=file_location, message_id=message_id, sender_id=sender_id, recipient_id=recipient_id)
+        db_file = File(filename=file.filename, file_url=UPLOAD_DIRECTORY, message_id=message_id, sender_id=current_user.id, recipient_id=recipient_id)
         session.add(db_file)
         await session.commit()
         await session.refresh(db_file)
@@ -50,3 +46,9 @@ async def get_files(current_user: User = Depends(get_current_user)):
         result = await session.execute(select(File).filter(or_(File.sender_id == current_user.id, File.recipient_id == current_user.id)))
         files = result.scalars().all()
         return files
+
+
+@router.get("/download-file/{file_name}")
+async def download_file(file_name: str):
+    file_path = os.path.join(UPLOAD_DIRECTORY, file_name)
+    return file_path
